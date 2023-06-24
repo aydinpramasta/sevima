cfd356279d61ed1a84bcebaf8aee09571fa9fe96
Aydin Pramasta <aydin.pram@gmail.com>
2023-06-24 23:58:12 +0700

    improve ux in navigation

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'home');
+Route::view('/', 'home')->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
